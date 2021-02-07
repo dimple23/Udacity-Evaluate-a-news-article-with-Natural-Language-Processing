8238149cde8259461feb6787e9b049cab774c9be
dimple23 <dimplechavda23@gmail.com>
2021-02-07 18:30:10 -0500

    added

---
+++ dist/precache-manifest.682da29c8dc1e3d570eefaf138ed9b14.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829d55185ec06063ad3f85108c9fc447",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c01b27fcf66939b1aa28",
+    "url": "bundle.min.js"
+  },
+  {
+    "revision": "c01b27fcf66939b1aa28",
+    "url": "main.css"
+  },
+  {
+    "url": "precache-manifest.9e0a8e9d85c8146514074d4dea32afd2.js"
+  },
+  {
+    "revision": "35d940e44c7a1081e30058ab7806645b",
+    "url": "service-worker.js"
+  }
+]);
---
+++ dist/precache-manifest.9e0a8e9d85c8146514074d4dea32afd2.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01b27fcf66939b1aa28",
+    "url": "bundle.min.js"
+  },
+  {
+    "revision": "c01b27fcf66939b1aa28",
+    "url": "main.css"
+  }
+]);
